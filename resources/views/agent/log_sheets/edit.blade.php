aba9aaf364a607d2217dc1ec2c6f655c9375c0ef
sysadmin29 <sysadmin@cccdms.com>
2018-07-14 12:42:55 +0800

    editing log sheet edit form

--- resources/views/agent/log_sheets/edit.blade.php
+++ resources/views/agent/log_sheets/edit.blade.php
@@ -2,9 +2,6 @@
 
 @section('css')
     <style>
-        .table-hover>tfoot>tr>td{
-            padding:0;
-        }
     </style>
 @endsection
 
@@ -39,12 +36,12 @@
                 <tr>
                     <th>Entry Date</th>
                     <th>S / R</th>
-                    <th>Operator</th>
-                    <th>Batch ID</th>
-                    <th>Start</th>
-                    <th>End</th>
-                    <th>Total Hours</th>
-                    <th>Records</th>
+                    <th align="center">Operator</th>
+                    <th align="center">Batch ID</th>
+                    <th align="center">Start</th>
+                    <th align="center">End</th>
+                    <th align="center">Total Hours</th>
+                    <th width="80px">Records</th>
                     <th>Status</th>
                     <th>Remarks</th>
                     <th>Action</th>
@@ -53,48 +50,78 @@
                 <tbody>
                     @foreach($download->log_sheet as $log)
                         <tr class="{{ $log->status == 'Finished' ? 'success' : '' }}">
-                            <td>{{ $log->entry_date }}</td>
-                            <td>{{ $log->sale_rent }}</td>
-                            <td>{{ $log->user_id }}</td>
-                            <td>{{ $log->batch_id }}</td>
-                            <td>{{ $log->start_time }}</td>
-                            <td>{{ $log->end_time }}</td>
-                            <td>{{ $log->total_time }}</td>
-                            <td align="right" width="40px">{{ $log->records }}</td>
-                            <td><small class="label label-{{ $log->status == 'Finished' ? 'success' : 'danger' }}">{{ $log->status}}</small></td>
-                            <td>{{ $log->remarks }}</td>
-                            <td>
-                                @if($log->user_id == auth()->user()->id)
-                                    @if($log->end_time == '00:00:00')
-                                        {!! Form::model($download,['method'=>'PATCH','url' => '/agent/entries/'.$log->id,'class'=>'form-inline']) !!}
-                                        {!! Form::submit('Stop',['class'=>'btn btn-danger input-sm']) !!}
-                                        {!! Form::close() !!}
+                            <td align="center">{{ $log->entry_date }}</td>
+                            <td align="center">{{ $log->sale_rent }}</td>
+                            <td align="center">{{ $log->user_id }}</td>
+                            <td align="center">{{ $log->batch_id }}</td>
+                            <td align="center">{{ $log->start_time }}</td>
+                            <td align="center">{{ $log->end_time }}</td>
+                            <td align="center">{{ $log->total_time }}</td>
+                            {!! Form::model($download,['method'=>'PATCH','url' => '/agent/entries/'.$log->id,'class'=>'form-inline']) !!}
+                                <td align="right">
+                                    @if($log->end_time != '00:00:00')
+                                        {{ $log->records }}
+                                    @else
+                                        {!! Form::text('records',$log->records,['class'=>'form-control input-sm','required'=>'true']) !!}
                                     @endif
-                                @endif
-                            </td>
+                                </td>
+                                <td align="center">
+                                    @if($log->end_time != '00:00:00')
+                                        <small class="label label-{{ $log->status == 'Finished' ? 'success' : 'danger' }}">{{ $log->status }}</small>
+                                    @else
+                                        {!! Form::select('status',['Finished'=>'FIN','Unfinished'=>'UNF'],null,['class'=>'form-control input-sm','required'=>'true']) !!}
+                                    @endif
+                                </td>
+                                <td>
+                                    @if($log->end_time != '00:00:00')
+                                        <small class="label label-info">{{ $log->remarks }}</small>
+                                    @else
+                                        {!! Form::text('remarks',$log->remarks,['class'=>'form-control input-sm','placeholder'=>'Remarks']) !!}
+                                    @endif
+
+                                </td>
+
+
+                                <td align="center">
+                                    @if($log->user_id == auth()->user()->id)
+                                        @if($log->end_time == '00:00:00')
+                                            {!! Form::submit('Stop',['class'=>'btn btn-danger input-sm']) !!}
+                                        @endif
+                                    @endif
+                                </td>
+                            {!! Form::close() !!}
                         </tr>
                     @endforeach
                 </tbody>
                 <tfoot>
                     <tr>
-                        {!! Form::model($download,['method'=>'PUT','url' => '/agent/entries/'.$download->id,'class'=>'form-horizontal']) !!}
-                            {{ csrf_field() }}
-                        <td>
-                            {!! Form::hidden('entry_date',\Carbon\Carbon::now()->format('Y-m-d'),['class'=>'form-control input-sm','readonly'=>'true']) !!}
-                        </td>
-                        <td colspan="2">
-                            {!! Form::select('sale_rent',['Sale'=>'Sale','Rent'=>'Rent'],null,['class'=>'form-control input-sm','required'=>'true']) !!}
-                        </td>
-                        <td>
-                            {!! Form::text('batch_id',null,['class'=>'form-control input-sm','required'=>'true','pattern' => "[a-zA-Z0-9]{3}[_][0-9]{8}[_][sS|Rr][_][0-9]{2}"]) !!}
-                        </td>
-                        <td>
-                            {!! Form::text('remarks',null,['class'=>'form-control input-sm','placeholder'=>'Remarks']) !!}
-                        </td>
-                        <td>
-                            <button type="submit" class="btn btn-primary btn-block btn-flat input-sm"><i class="fa fa-plus"></i>Start</button>
-                        </td>
-                        {!! Form::close() !!}
+                        @if($download->log_sheet->count() == 0)
+                            <td colspan="11">
+                                {!! Form::model($download,['method'=>'PUT','url' => '/agent/entries/'.$download->id,'class'=>'form-inline']) !!}
+                                {{ csrf_field() }}
+                                {!! Form::hidden('entry_date',\Carbon\Carbon::now()->format('Y-m-d'),['class'=>'form-control input-sm','readonly'=>'true']) !!}
+                                {!! Form::select('sale_rent',['Sale'=>'Sale','Rent'=>'Rent'],null,['class'=>'form-control input-sm','required'=>'true']) !!}
+                                {!! Form::text('batch_id',null,['class'=>'form-control input-sm','required'=>'true','pattern' => "[a-zA-Z0-9]{3}[_][0-9]{8}[_][sS|Rr][_][0-9]{2}"]) !!}
+                                {!! Form::text('remarks',null,['class'=>'form-control input-sm','placeholder'=>'Remarks']) !!}
+                                {!! Form::submit('Start Entry',['class'=>'btn btn-success input-sm']) !!}
+                                {!! Form::close() !!}
+                            </td>
+                        @else
+                            @if($log->end_time != '00:00:00')
+                            <td colspan="11">
+                                <div class="form-group pull-right">
+                                    {!! Form::model($download,['method'=>'PUT','url' => '/agent/entries/'.$download->id,'class'=>'form-inline']) !!}
+                                    {{ csrf_field() }}
+                                    {!! Form::hidden('entry_date',\Carbon\Carbon::now()->format('Y-m-d'),['class'=>'form-control','readonly'=>'true']) !!}
+                                    {!! Form::select('sale_rent',['Sale'=>'Sale','Rent'=>'Rent'],null,['class'=>'form-control','required'=>'true']) !!}
+                                    {!! Form::text('batch_id',null,['class'=>'form-control','required'=>'true','pattern' => "[a-zA-Z0-9]{3}[_][0-9]{8}[_][sS|Rr][_][0-9]{2}"]) !!}
+                                    {!! Form::text('remarks',null,['class'=>'form-control','placeholder'=>'Remarks']) !!}
+                                    {!! Form::submit('Start Entry',['class'=>'btn btn-success']) !!}
+                                    {!! Form::close() !!}
+                                </div>
+                            </td>
+                            @endif
+                        @endif
                     </tr>
                 </tfoot>
             </table>
