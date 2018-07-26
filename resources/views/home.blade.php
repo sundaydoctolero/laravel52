c6fb9569e9fada67979eeb305d8e9e8273cca482
sysadmin29 <sysadmin@cccdms.com>
2018-07-26 13:09:30 +0800

    fixed slide show

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -129,7 +129,29 @@
         </div>
     </div>
 
-    
+    <!-- Slideshow container -->
+    <div class="slideshow-container">
+        <!-- Full-width images with number and caption text -->
+        @foreach($images as $image)
+            <div class="mySlides fade">
+                <div class="numbertext">{{ $image->id }} / {{  $images->count() }}</div>
+                <img class=".img-thumbnail" src="{{ asset('/images/background/'.$image->image_name) }}">
+                <div class="text">{{ $image->caption }}</div>
+            </div>
+        @endforeach
+        <!-- Next and previous buttons -->
+        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+        <a class="next" onclick="plusSlides(1)">&#10095;</a>
+    </div>
+    <br>
+
+    <!-- The dots/circles -->
+    <div style="text-align:center">
+        <span class="dot" onclick="currentSlide(1)"></span>
+        <span class="dot" onclick="currentSlide(2)"></span>
+        <span class="dot" onclick="currentSlide(3)"></span>
+    </div>
+
 
 
 @endsection
