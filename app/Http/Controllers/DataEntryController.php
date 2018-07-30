15a7925049835cdaa2f86fbb9061e245d6cd9276
sysadmin29 <sysadmin@cccdms.com>
2018-07-31 07:19:12 +0800

    fixed DataEntryController edit

--- app/Http/Controllers/DataEntryController.php
+++ app/Http/Controllers/DataEntryController.php
@@ -30,26 +30,9 @@ class DataEntryController extends Controller
 
     public function edit(Download $download){
         //$download->lockForUpdate()->get(); //database level
-        if($download->locked_by == 0){
-            if($download->no_of_batches == 1){
-                $download->update(['locked_by' => auth()->user()->id]);
-            }
-        } else {
-            if($download->locked_by != auth()->user()->id){
-                return redirect()->back();
-            }
-        }
-
-        $download->load(['log_sheet' => function($query){
-            $query->where('user_id',auth()->user()->id)->get();
-        }]);
-
-        $log_sheets = Logsheet::where('download_id',$download->id)
-            ->where('user_id','<>',auth()->user()->id)
-            ->get();
-
+        $download->load('log_sheet');
+        $log_sheets = Logsheet::where('download_id',$download->id)->get();
         return view($this->view_path.'.edit',compact('download','log_sheets'));
-
     }
 
     public function update(Download $download,DownloadRequest $request){
