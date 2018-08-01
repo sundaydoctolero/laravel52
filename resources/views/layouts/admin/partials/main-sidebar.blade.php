a436e98a82dbe0dc413a231de9c6d938f1c6525b
sysadmin29 <sysadmin@cccdms.com>
2018-08-01 09:04:24 +0800

    fixed publication report

--- resources/views/layouts/admin/partials/main-sidebar.blade.php
+++ resources/views/layouts/admin/partials/main-sidebar.blade.php
@@ -61,23 +61,13 @@
                             </a>
                             <ul class="treeview-menu">
                                 <li class="{{ setActive('newspaper_reports') }}"><a href="/newspaper_reports/not_updated_reports"><i class="fa fa-arrow-down"></i> <span>Not Updated</span></a></li>
-                                <li class="{{ setActive('newspaper_reports') }}"><a href="/newspaper_reports/delivered_reports"><i class="fa fa-arrow-down"></i> <span>Delivered</span></a></li>
                                 <li class="{{ setActive('newspaper_reports') }}"><a href="/newspaper_reports/generate_pub_details"><i class="fa fa-arrow-down"></i> <span>Generate Publication Details</span></a></li>
-                                <li class="{{ setActive('newspaper_reports') }}"><a href="/newspaper_reports/download"><i class="fa fa-arrow-down"></i> <span>Download Count</span></a></li>
+                                <li class="{{ setActive('newspaper_reports') }}"><a href="/newspaper_reports/download"><i class="fa fa-arrow-down"></i> <span>Publication Reports</span></a></li>
                             </ul>
                         </li>
-
-
-
-
-
-
                     </ul>
                 </li>
             @endif
-
-
-
 
             <li class="{{ setActive('departments') }}"><a href="/departments"><i class="fa fa-arrow-down"></i> <span>Departments</span></a></li>
             <li class="{{ setActive('employees') }}"><a href="/departments"><i class="fa fa-arrow-down"></i> <span>Employees</span></a></li>
