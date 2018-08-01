ea83204b7abcc46e9b558b2d006264b7748a6956
sysadmin29 <sysadmin@cccdms.com>
2018-08-01 12:13:05 +0800

    added auto import console command

--- app/Console/Commands/BatchImport.php
+++ app/Console/Commands/BatchImport.php
@@ -4,6 +4,11 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 
+use Carbon\Carbon;
+use App\Download;
+use App\Publication;
+use App\Output;
+
 class BatchImport extends Command
 {
     /**
@@ -37,8 +42,46 @@ class BatchImport extends Command
      */
     public function handle()
     {
-        $x = new DownloadImportController();
-        $x->import_downloads();
-        echo 'successfull';
+        $today = Carbon::now();
+
+        $check = Download::where('publication_date',$today->toDateString())->get();
+
+        if($check->count() > 0){
+            echo "failed";
+        } else {
+            $publications =  Publication::whereHas('days',function ($query) use ($today) {
+                $query->where('day_name',$today->format('l'));
+            })->with('days')->get();
+
+            foreach($publications as $publication){
+                $download = new Download();
+                $download->publication_id = $publication->id;
+                $download->publication_date = $today->toDateString();
+                $download->no_of_batches = $publication->default_batch;
+                $download->status = 'For Download';
+                $download->save();
+                $download->output()->save(new Output());
+            }
+
+            if(Carbon::now()->day == 1){
+                $publications =  Publication::whereHas('days',function ($query) {
+                    $query->where('day_name','Monthly');
+                })->with('days')->get();
+
+                foreach($publications as $publication){
+                    $download = new Download();
+                    $download->publication_id = $publication->id;
+                    $download->publication_date = Carbon::now()->toDateString();
+                    $download->no_of_batches = $publication->default_batch;
+                    $download->status = 'For Download';
+                    $download->save();
+                    $download->output()->save(new Output());
+                }
+            }
+
+            echo "successful!!";
+
+        }
+
     }
 }
