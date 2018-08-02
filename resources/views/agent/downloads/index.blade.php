e09a6f4df9f0b1a007f32db713f9dfe801c343b3
sysadmin29 <sysadmin@cccdms.com>
2018-08-03 07:07:26 +0800

    fixed some download interface

--- resources/views/agent/downloads/index.blade.php
+++ resources/views/agent/downloads/index.blade.php
@@ -5,7 +5,12 @@
     <div class="col-xs-12">
         <div class="box">
             <div class="box-header">
-                <h1 class="box-title"><i class="fa fa-arrow-down"></i><b> Downloads</b> </h1>
+                <h1 class="box-title"><i class="fa fa-arrow-down"></i><b> Downloads </b>
+                    <small class="label label-success">{{ $downloads->where('status','For Download')->count() }}</small>
+                    <small class="label label-danger">{{ $downloads->where('status','Not Updated')->count() }}</small>
+                    <small class="label label-info">{{ $downloads->where('status','For Query')->count() }}</small>
+                    <small class="label label-warning">{{ $downloads->where('status','Pending')->count() }}</small>
+                </h1>
 
                 <div class="box-tools">
                     <div class="input-group input-group-sm" style="width: 150px;">
@@ -18,37 +23,27 @@
                     </div>
                 </div>
             </div>
-            <!-- /.box-header -->
             <div class="box-body table-responsive no-padding">
                 <table class="table table-hover">
                     <tbody><tr>
-                        <th>ID</th>
                         <th>Publication Name</th>
                         <th>Publication Date</th>
                         <th>Status</th>
                         <th>Pages</th>
                         <th>Remarks</th>
-                        <th>Ongoing Download By</th>
+                        <th>Downloader</th>
                         <th>Checked By</th>
                         <th>Action</th>
                     </tr>
                     @foreach($downloads as $download)
                     <tr>
-                        <td>{{ $download->id }}</td>
                         <td>{{ $download->publication->publication_name }}</td>
-                        <td>{{ $download->publication_date }}</td>
-                        <td>{{ $download->status }}</td>
+                        <td class="text-center">{{ $download->publication_date }}</td>
+                        <td class="text-center">{{ $download->status }}</td>
                         <td>{{ $download->pages }}</td>
                         <td>{{ $download->remarks }}</td>
-                        <td>
-
-                                <small class="label label-success">{{ $download->operator_locked['operator_no'] }}</small>
-
-                        </td>
-                        <td>
-                                <small class="label label-success">{{ $download->operator['operator_no'] }}</small>
-
-                        </td>
+                        <td class="text-center"><small class="label label-success">{{ $download->operator_locked['operator_no'] }}</small></td>
+                        <td class="text-center"><small class="label label-info">{{ $download->operator['operator_no'] }}</small></td>
                         <td>
                             <a href="/agent/downloads/{{ $download->id }}/edit"><button type="button" class="btn btn-primary btn-sm"><i class="fa fa-edit"></i> Locked</button></a>
                         </td>
