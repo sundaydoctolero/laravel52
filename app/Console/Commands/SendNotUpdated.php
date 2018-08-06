7d5bc4af36e1855eab89859ed62210b06568fb22
sysadmin29 <sysadmin@cccdms.com>
2018-08-06 11:25:26 +0800

    added auto email of not updated

---
+++ app/Console/Commands/SendNotUpdated.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+use App\Download;
+use Mail;
+use Carbon\Carbon;
+
+class SendNotUpdated extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'download:not_updated';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Auto Email Not Updated Publications';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $downloads = Download::whereIn('status',['For Download','Not Updated','For Query','Pending'])->orderBy('status')->orderBy('publication_date')->get();
+        $downloads->load('publication','operator');
+
+        Mail::send(['html'=>'mail.not_updated'],
+            ['downloads'=>$downloads],
+            function($message){
+                $message->to(['sysadmin@cccdms.com','ccc.news@cccdms.com','garrys@cccdms.com','tessb@cccdms.com'],'LinkMe Systems')
+                    ->subject('Not Updated Report as of '.Carbon::now()->toDateString());
+            });
+
+        echo "Successfull!!";
+    }
+}
---
+++ resources/views/mail/not_updated.blade.php
@@ -0,0 +1,54 @@
+<html>
+<head>
+    <style>
+        table {
+            font-family: arial, sans-serif;
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        td, th {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 8px;
+        }
+
+        tr:nth-child(even) {
+            background-color: #dddddd;
+        }
+    </style>
+</head>
+<body>
+    <h2>For Download : {{ $downloads->where('status','For Download')->count() }}</h2>
+    <h2>Not Updated : {{ $downloads->where('status','Not Updated')->count() }}</h2>
+    <h2>For Query : {{ $downloads->where('status','For Query')->count() }}</h2>
+    <h2>Not Updated : {{ $downloads->where('status','Pending')->count() }}</h2>
+    <h2>Total : {{ $downloads->count() }}</h2>
+
+    <table>
+        <tr bgcolor="#FF0000">
+            <th>ID</th>
+            <th>Publication Name</th>
+            <th align="center">Publication Date</th>
+            <th>Status</th>
+            <th>Remarks</th>
+            <th>Last Checked By</th>
+        </tr>
+        @if($downloads)
+            @foreach($downloads as $count => $download)
+                <tr>
+                    <td>{{ $count++ + 1 }}</td>
+                    <td>{{ $download->publication->publication_name }}</td>
+                    <td align="center">{{ $download->publication_date }}</td>
+                    <td>{{ $download->status }}</td>
+                    <td>{{ $download->remarks }}</td>
+                    <td>{{ $download->checked_by == 0 ? '' : $download->operator->operator_no }}</td>
+                </tr>
+            @endforeach
+        @endif
+    </table>
+
+    <p>This is an auto generated email. Please do not reply.</p>
+
+</body>
+</html>
