c82805cf25c676f52eb338dd8de540b1667f2db7
sysadmin29 <sysadmin@cccdms.com>
2018-09-25 11:23:52 +0800

    created ImportDTR console command

---
+++ app/Console/Commands/ImportDTR.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\DailyTimeRecord;
+
+class ImportDTR extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'import:dtr';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $storage_path = base_path().'/storage/dtr_upload';
+
+        if(($handle = fopen($storage_path.'/'.'20180924-20180924.txt','r')) !== false  ){
+            while(($data= fgetcsv($handle,100000,"\t")) !== false){
+                $dtr = new DailyTimeRecord();
+                $dtr->operator_no = substr($data[0],0,3);
+                $dtr->dtr_date = substr($data[0],27,4).'-'.substr($data[0],21,2).'-'.substr($data[0],24,2); //2018-09-22
+                $dtr->dtr_time = substr($data[0],31,9);
+                $dtr->dtr_code = substr($data[0],41,1);
+                $dtr->device_id = substr($data[0],43,1);
+                $dtr->remarks = "";
+                $dtr->save();
+            }
+        }
+
+        echo "Success!!";
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,6 +15,7 @@ class Kernel extends ConsoleKernel
     protected $commands = [
          Commands\BatchImport::class,
          Commands\SendNotUpdated::class,
+        Commands\ImportDTR::class,
     ];
 
     /**
