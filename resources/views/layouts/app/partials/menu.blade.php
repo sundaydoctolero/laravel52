a39612cf08be523bf8af4c8e7cc42ea35ccd20af
sysadmin29 <sysadmin@cccdms.com>
2018-09-25 15:25:54 +0800

    fixed accesscontrol to agent app

--- resources/views/layouts/app/partials/menu.blade.php
+++ resources/views/layouts/app/partials/menu.blade.php
@@ -4,10 +4,13 @@
         <li class='{{ setActive('/')}}'> <a href="/"><i class="fa fa-home"></i> Home</a></li>
         <li class='{{ setActive('myprofile*') }}'><a href="/myprofile/{{ auth()->user()->id }}/edit"><i class="fa fa-user"></i> Profile </a></li>
         <li class='{{ setActive('agent/tsheets')}}'><a href="/agent/tsheets"><i class="fa fa-file-excel-o"></i> Tsheet </a></li>
-        <li class='{{ setActive('agent/downloads')}}'><a href="/agent/downloads"><i class="fa fa-download"></i> Download </a></li>
-        <li class='{{ setActive('agent/entries')}}'><a href="/agent/entries"><i class="fa fa-keyboard-o"></i> Entry </a></li>
-        <li class='{{ setActive('agent/outputs')}}'><a href="/agent/outputs"><i class="fa  fa-file-text-o"></i> Output </a></li>
-        <li class='{{ setActive('agent/deliveries')}}'><a href="/agent/deliveries?date_from={{\Carbon\Carbon::now()->toDateString()}}"><i class="fa fa-folder"></i> Delivered </a></li>
+
+        @if(auth()->user()->employee->department_id == 1 || auth()->user()->employee->department_id == 3 || auth()->user()->employee->department_id == 4)
+            <li class='{{ setActive('agent/downloads')}}'><a href="/agent/downloads"><i class="fa fa-download"></i> Download </a></li>
+            <li class='{{ setActive('agent/entries')}}'><a href="/agent/entries"><i class="fa fa-keyboard-o"></i> Entry </a></li>
+            <li class='{{ setActive('agent/outputs')}}'><a href="/agent/outputs"><i class="fa  fa-file-text-o"></i> Output </a></li>
+            <li class='{{ setActive('agent/deliveries')}}'><a href="/agent/deliveries?date_from={{\Carbon\Carbon::now()->toDateString()}}"><i class="fa fa-folder"></i> Delivered </a></li>
+        @endif
         <li class='{{ setActive('agent/tasks') }}'><a href="/agent/tasks"><i class="fa fa-tasks"></i> Tech </a></li>
     </ul>
 </div>
